a8ab7f2defa802d5a6be5f85d1261d53bad3b47f
DanielS <designdoesdaniel@gmail.com>
2025-05-02 14:50:58 +1200

    added chain op
    BUG: No order-of-operation. Need to move onto full shunt algo

--- src/main.cpp
+++ src/main.cpp
@@ -4,50 +4,39 @@
 #include <iostream>
 #include <string>
 
-
-int main() {
-	Calculate calc;
+int main()
+{
+    Calculate calc;
     std::string expression = "";
-	std::string input;
-	bool haveResult = false;
+    std::string input;
+    bool haveResult = false;
 
+    while (!haveResult)
+    {
+        std::cout << "Amend string with (or press leave blank to compute):" << std::endl;
+        std::getline(std::cin, input);
 
-	while (!haveResult)
-	{
-	std::cout << "Amend string with (or press leave blank to compute):" << std::endl;
-	std::getline(std::cin, input);
+        expression += input;
+        std::cout << expression << std::endl;
 
+        if (input.empty())
+        {
+            haveResult = true;
+            std::cout << "Empty input. Exiting loop." << std::endl;
+        }
+    }
 
-	expression += input;
-	std::cout << expression << std::endl;
-
-		if (input.empty())
-		{
-			haveResult = true;
-			std::cout << "Empty input. Exiting loop." << std::endl;
-		}
-	}
-
-	float result = calc.process(expression); // call the method
-	std::cout << result << std::endl;
+    float result = calc.process(expression); // call the method
+    std::cout << result << std::endl;
 
     return 0;
-
-
 }
-
-
-
-
-
-
-
 
 /*
 int main()
 {
     sf::Window window(sf::VideoMode({300, 600}), "My window");
-	Calculate calculator;
+    Calculate calculator;
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -59,8 +48,8 @@ int main()
             if (event->is<sf::Event::Closed>())
                 window.close();
 
-			render();
-			processInput();
+            render();
+            processInput();
         }
     }
 }
@@ -68,7 +57,7 @@ int main()
 
 void render()
 {
-	
+
 }
 
 void processInput()
